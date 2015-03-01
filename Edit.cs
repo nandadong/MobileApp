3990c54ffa9df1341c57e431e753486d0f5e430d
kdody <kdody@vtedu>
2015-03-01 17:30:32 -0500

    Room Control for iOS and Android
    Rooms: List of all the rooms
    Edit: Edit what electronics are in the room
    Add: screen that allows the user to add electronics

---
+++ Edit.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+using Xamarin.Forms;
+
+namespace RoomControl
+{
+	public class Edit : ContentPage
+	{
+		public Edit (/*RoomName k*/)
+		{
+			/*Content = new StackLayout { 
+				Children = {
+					new Label { Text = "Kitchen" } //this name will change depending on the button pressed
+				}
+			};*/
+			var next = new Button { Text = "Edit" };
+			next.Clicked += async (sender, e) => {
+				await Navigation.PushAsync (new Add());
+			};
+			var white = new Label
+			{
+				Text = /*k.Name*/"Kitchen", //this will change depending on the room clicked
+				//BackgroundColor = Color.White,
+				XAlign = TextAlignment.Center,
+				//Font = Font.SystemFontOfSize (20)
+			};
+			var listView = new ListView
+			{
+				RowHeight = 40
+			};
+			listView.ItemsSource = new RoomName [] {
+				new RoomName {Name = "Light #1"},
+				new RoomName {Name = "Light #2"}
+			};
+
+			listView.ItemTemplate = new DataTemplate(typeof(TextCell));
+			listView.ItemTemplate.SetBinding(TextCell.TextProperty, "Name");
+
+			var b = new Button { Text = "Edit" };
+			b.Clicked += async (sender, e) => {
+				await Navigation.PushAsync (new Add());
+			};
+
+			//listView.ItemSelected += async (sender, e) => {
+				//(ListView(sender)).SelectedItem = a
+
+			//};
+
+			//MainPage = new ContentPage {
+				Content = new StackLayout {
+					VerticalOptions = LayoutOptions.FillAndExpand,
+					Padding = new Thickness (20),
+					Children = { white, listView, b }
+			};
+		//}
+	}
+}
+
+}
---
+++ RoomControl.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+using Xamarin.Forms;
+//using Xamarin.Forms.iNavigation;
+
+namespace RoomControl
+{
+
+	public class RoomName {
+		public string Name { get; set; }
+		public bool Done { get; set; }
+		//public string [15];
+	}
+	public class App : Xamarin.Forms.Application
+	{
+		public static Page GetMainPage()
+		{
+			var mainNav = new NavigationPage(new Rooms());
+			return mainNav;
+		}
+		public App ()
+		{
+			var np = new NavigationPage (new Rooms()) {Title="Room Nav"};
+
+			// The root page of your application
+			MainPage = np;
+
+			//listView.ItemTemplate = new DataTemplate(typeof(TextCell));
+			//listView.ItemTemplate.SetBinding(TextCell.TextProperty, "Name");
+
+
+		}
+
+		protected override void OnStart ()
+		{
+			// Handle when your app starts
+
+		}
+
+		protected override void OnSleep ()
+		{
+			// Handle when your app sleeps
+		}
+
+		protected override void OnResume ()
+		{
+			// Handle when your app resumes
+		}
+	}
+}
+
