a78e2711dbc6e3f287c44847d0cb65aa794a0ed8
kdody <kdody@vtedu>
2015-03-01 21:04:21 -0500

    Room Control
    RoomList->RoomEdit->RoomAdd
    The lists are hard coded in currently.

---
+++ RoomListView.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace HomeAutomationApp
+{
+	public partial class RoomListView : ContentPage
+	{
+		public RoomListView ()
+		{
+			var white = new Label
+			{
+				Text = "ROOMS",
+				BackgroundColor = Color.White,
+				XAlign = TextAlignment.Center,
+				//Font = Font.SystemFontOfSize (20)
+			};
+			var listView = new ListView
+			{
+				RowHeight = 40
+			};
+			listView.ItemsSource = new string [] {
+				"Kitchen",
+				"Hall",
+				"Living Room",
+				"Bathroom #1",
+				"Bedroom #1"
+			};
+
+			//listView.ItemTemplate = new DataTemplate(typeof(TextCell));
+			//listView.ItemTemplate.SetBinding(TextCell.TextProperty, "Name");
+
+			Content = new StackLayout {
+				//VerticalOptions = LayoutOptions.FillAndExpand,
+				Padding = new Thickness (20),
+				Children = { white, listView }
+			};
+
+			listView.ItemSelected += async (sender, e) => {
+				//RoomName k = listView.ItemsSource,
+				//await Navigation.PushAsync(new Edit(k)); 
+				await Navigation.PushAsync (new RoomsEditView());
+			};
+		}
+	}
+}
+
---
+++ RoomsEditView.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace HomeAutomationApp
+{
+	public partial class RoomsEditView : ContentPage
+	{
+		public RoomsEditView ()
+		{
+			var next = new Button { Text = "Edit" };
+			next.Clicked += async (sender, e) => {
+				await Navigation.PushAsync (new RoomsAddView());
+			};
+			var white = new Label
+			{
+				Text = /*k.Name*/"Kitchen", //this will change depending on the room clicked
+				//BackgroundColor = Color.White,
+				XAlign = TextAlignment.Center,
+				//Font = Font.SystemFontOfSize (20)
+			};
+			var listView = new ListView
+			{
+				RowHeight = 40
+			};
+			listView.ItemsSource = new string [] {
+				"Light #1",
+				"Light #2"
+			};
+
+			//listView.ItemTemplate = new DataTemplate(typeof(TextCell));
+			//listView.ItemTemplate.SetBinding(TextCell.TextProperty, "Name");
+
+			var b = new Button { Text = "Edit" };
+			b.Clicked += async (sender, e) => {
+				await Navigation.PushAsync (new RoomsAddView());
+			};
+
+			//MainPage = new ContentPage {
+			Content = new StackLayout {
+				//VerticalOptions = LayoutOptions.FillAndExpand,
+				Padding = new Thickness (20),
+				Children = { white, listView, b }
+			};
+		}
+	}
+}
+
