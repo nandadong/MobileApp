b25c169575d9d9c87430b5de526ad595d6a62f91
nathodius <njh2986@vt.edu>
2015-03-01 19:56:09 -0500

    Add LoginView to HomeAutomationApp/View

---
+++ HomeAutomationApp/View/LoginView.xaml.cs
@@ -0,0 +1,26 @@
+﻿/*
+ * File: HomeAutomationApp/View/LoginView.xaml.cs
+ * Accompanying XAML file: HomeAutomationApp/View/LoginView.xaml
+ * Author: Nate Hughes
+ * Shipped: 03-01-2015
+ */
+
+using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace HomeAutomationApp
+{
+	public partial class LoginView : ContentPage
+	{
+		public LoginView ()
+		{
+			//init the UI
+			InitializeComponent ();
+			 
+			Title = "Login";
+		}
+	}
+}
+
