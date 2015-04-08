2bc4ae53af69c7d80f610850a967d26e1693c43a
Brian Kaplan <bakaplan2@gmail.com>
2015-04-07 21:30:15 -0400

    Stubbed GCM PushNotifications
    Implementation is far from complete, Registering with GCM needs to be
    implemented. Intents need to be handled. Correct BrodadcastReciever
    Events need to be implemented.

---
+++ Droid/GCMIntentService.cs
@@ -0,0 +1,61 @@
+﻿using System;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+
+namespace HomeAutomationApp.Droid
+{
+	[Service]
+	public class MyIntentService : IntentService
+	{
+		static PowerManager.WakeLock sWakeLock;
+		static object LOCK = new object();
+
+		static void RunIntentInService(Context context, Intent intent)
+		{
+			lock (LOCK)
+			{
+				if (sWakeLock == null)
+				{
+					// This is called from BroadcastReceiver, there is no init.
+					var pm = PowerManager.FromContext(context);
+					sWakeLock = pm.NewWakeLock(
+						WakeLockFlags.Partial, "My WakeLock Tag");
+				}
+			}
+
+			sWakeLock.Acquire();
+			intent.SetClass(context, typeof(MyIntentService));
+			context.StartService(intent);
+		}
+
+		protected override void OnHandleIntent(Intent intent)
+		{
+			try
+			{
+				Context context = this.ApplicationContext;
+				string action = intent.Action;
+
+				if (action.Equals("com.google.android.c2dm.intent.REGISTRATION"))
+				{
+					//HandleRegistration(context, intent);
+				}
+				else if (action.Equals("com.google.android.c2dm.intent.RECEIVE"))
+				{
+					//HandleMessage(context, intent);
+				}
+			}
+			finally
+			{
+				lock (LOCK)
+				{
+					//Sanity check for null as this is a public method
+					if (sWakeLock != null)
+						sWakeLock.Release();
+				}
+			}
+		}
+	}
+}
+
