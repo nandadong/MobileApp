091c4f834911a43da3c3f10761095baf1d060e15
ba2669 <ba2669@vt.edu>
2015-04-13 16:13:26 -0400

    Added Unit Testing project for logic testing

---
+++ LogicUnitTests/Test.cs
@@ -0,0 +1,16 @@
+﻿using NUnit.Framework;
+using System;
+
+namespace LogicUnitTests
+{
+	[TestFixture ()]
+	public class Test
+	{
+		[Test ()]
+		public void TestCase ()
+		{
+			Assert.IsTrue (true);
+		}
+	}
+}
+
