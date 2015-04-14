b2a814c0441617b132b029f2c9ed82e86a56d84e
cmpadden <cmpadden@vt.edu>
2015-04-13 22:50:38 -0400

    updated code to handle button press and user input

--- HomeAutomationApp/View/AddDeviceView.xaml.cs
+++ HomeAutomationApp/View/AddDeviceView.xaml.cs
@@ -29,23 +29,100 @@ namespace HomeAutomationApp
 			foreach (var item in deviceList) {
 				devicePicker.Items.Add(( item ?? "" ).ToString());
 			}
-				
+		
+
+			// create entries for device name and room id
+			var deviceEntry = new Entry();
+			var roomEntry = new Entry ();
+			deviceEntry.Placeholder = "New Device Name";
+			roomEntry.Placeholder = "New Room ID";
+
+			// create button for pushing device settings
+			var deviceButton = new Button();
+			deviceButton.Text = "Update Device";
+			deviceButton.TextColor = Color.White;
+			deviceButton.BackgroundColor = Color.FromHex ("77D065");
+
+			// label to display successful push
+			var confirmationLabel = new Label ();
+			confirmationLabel.Text = "";
+			confirmationLabel.TextColor = Color.White;
+
 			// add content to the view
 			Content = new StackLayout {
 				Spacing = 20, Padding = 20,
 				VerticalOptions = LayoutOptions.Center,
 				Children = {
 					devicePicker,
-					new Entry { Placeholder = "New Device Name" },
-					new Entry { Placeholder = "New Room ID" },
-					new Button {
-						Text = "Update Device",
-						TextColor = Color.White,
-						BackgroundColor = Color.FromHex ("77D065")
-					}
+					deviceEntry,
+					roomEntry,
+					deviceButton,
+					confirmationLabel
 				}
 			};
+
+			// handling of button press
+			deviceButton.Clicked += (object sender, EventArgs e) => {
+
+				if ( (deviceEntry.Text) != "" && (roomEntry.Text != "") ){
+					confirmationLabel.TextColor = Color.Green;
+					confirmationLabel.Text = "Success!\nNew device name: " + deviceEntry.Text + "\nNew room ID: " + roomEntry.Text + ".";
+
+
+					// user input can be referenced using:
+					// deviceEntry.Text
+					// roomEntry.Text
+
+				
+				} else {
+					confirmationLabel.TextColor = Color.Red;
+					confirmationLabel.Text = "You must enter a value for device name and room ID.";
+				}
+
+			};
+				
 		}
+
+
+		/*
+
+		//please make the spaceID and deviceName user inputs
+		const string blob =  
+		"{" +
+		"		deviceID : 5 ", +
+		"		DeviceName: \"light\","+
+		"		HouseID: 6,"+
+		"		SpaceID: 7,"+
+		"}";
+
+		PostDeviceAsync (JsonConvert.SerializeObject(blob)).Wait ();
+
+		public async Task<object> PostDeviceAsync (string packet) 
+		{
+
+			var client = new HttpClient ();
+			client.Timeout = TimeSpan.FromSeconds (10);
+
+			client.BaseAddress = new Uri(ConfigModel.Url);
+
+			try
+			{
+				var response = await client.PostAsync("api/dev/\\", 
+					new StringContent(packet, Encoding.UTF8, "application/json")).ConfigureAwait(false);
+
+				return response.StatusCode;
+
+			}
+			catch(Exception e)
+			{
+				Debug.WriteLine("HomeAutomationDebugError - Update Error: " + e.Message);
+				Debug.WriteLine("HomeAutomationDebugError - Position Update Error: " + e.InnerException.Message);
+			}
+
+			return null;
+		}
+
+		*/
 	}
 }
 
