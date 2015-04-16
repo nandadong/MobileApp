a0eaf116729be6d365ee1105973bd3324f94a297
Balaji <ba2669@vt.edu>
2015-04-15 23:46:35 -0400

    Updated initial code for external testing tool

---
+++ ExternalTestingApp/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ExternalTestingApp
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+			Console.WriteLine ("Hello World!");
+			Console.WriteLine ("This Application can be used to ensure if the Mobile app can" +
+			" be uploaded and opened on an Android device");
+
+
+
+		}
+	}
+}
