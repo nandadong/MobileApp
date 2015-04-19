881379b8f430e3ace1c15bd104481d44efb7e407
cmpadden <cmpadden@vt.edu>
2015-04-19 19:54:07 -0400

    automatically modified R.java file

--- Droid/obj/Debug/android/com/homeautomationapp/R.java
+++ Droid/obj/Debug/android/com/homeautomationapp/R.java
@@ -15,17 +15,20 @@ public final class R {
         public static final int mic=0x7f020001;
     }
     public static final class id {
-        public static final int GCMRegister=0x7f040006;
-        public static final int GCMUnregister=0x7f040007;
-        public static final int deviceID=0x7f040004;
-        public static final int linearLayout1=0x7f040005;
-        public static final int micButton=0x7f040000;
-        public static final int myTextBox=0x7f040001;
-        public static final int notifyBtn=0x7f040002;
-        public static final int textView1=0x7f040003;
+        public static final int GCMRegister=0x7f050006;
+        public static final int GCMUnregister=0x7f050007;
+        public static final int deviceID=0x7f050004;
+        public static final int linearLayout1=0x7f050005;
+        public static final int micButton=0x7f050000;
+        public static final int myTextBox=0x7f050001;
+        public static final int notifyBtn=0x7f050002;
+        public static final int textView1=0x7f050003;
     }
     public static final class layout {
         public static final int homelayout=0x7f030000;
         public static final int notificationlayout=0x7f030001;
+    }
+    public static final class string {
+        public static final int library_name=0x7f040000;
     }
 }
