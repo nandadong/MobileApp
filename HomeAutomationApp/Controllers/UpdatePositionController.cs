9b195764f292d289ea1da596db072d13f2ef6a59
Balaji Arun <balajiarun.dev@hotmail.com>
2015-04-22 16:32:30 -0400

    Misc Fixes

--- HomeAutomationApp/Controllers/UpdatePositionController.cs
+++ HomeAutomationApp/Controllers/UpdatePositionController.cs
@@ -16,20 +16,19 @@ public class UpdatePositionController
 
 	public async Task<HttpStatusCode> SendPositionAsync(string packet, string user)
 	{
-
-		var client = new HttpClient();
-		client.Timeout = TimeSpan.FromSeconds(2);
-
-		Debug.WriteLine("HAD: URL: " + ConfigModel.Url);
-		client.BaseAddress = new Uri(ConfigModel.Url);
-
 		try
 		{
+			var client = new HttpClient();
+			client.Timeout = TimeSpan.FromSeconds(10);
+
+			Debug.WriteLine("HAD: URL: " + ConfigModel.Url);
+			client.BaseAddress = new Uri(ConfigModel.Url);
+
+
 			var response = await client.PostAsync("api/app/user/updateposition/" + user, 
 				               new StringContent(packet, Encoding.UTF8, "application/json")).ConfigureAwait(false);
 
 			return response.StatusCode;
-
 		}
 		catch(Exception e)
 		{
