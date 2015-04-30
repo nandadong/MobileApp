976cbfaaba27225c1ec26773b00af2c509e759d2
Brian Kaplan <bakaplan2@gmail.com>
2015-04-29 21:03:23 -0400

    Added User Class, Updated GCM to Post to Server on Token change, some other fixes

---
+++ HomeAutomationApp/Models/User.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace HomeAutomationApp
+{
+public static class User
+{
+	private static string username;
+	private static byte[] password;
+
+
+	public static string getUsername()
+	{
+		return username;
+	}
+
+	public static void setUsername(string u)
+	{
+		username = u;	
+	}
+
+	public static void setPassword(string p)
+	{
+		password = GetHash(p);
+	}
+
+	public static byte[] getPassword()
+	{
+		return password;
+	}
+
+	public static byte[] GetHash(string inputString)
+	{
+		return null;
+	}
+}
+}
+
